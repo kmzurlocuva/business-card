ffed527d46ed91f9be7bb653da755ca6b1e9c4ac
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2016-01-21 14:51:09 -0800

    refining projects copy in JSON

--- js/routes.js
+++ js/routes.js
@@ -8,13 +8,6 @@ page('/about', aboutController.index);
 page();
 
 
-
-
-// function contact(ctx) {
-//   mylog1('Contact me at supercoder@supercoder.com<br>' +
-// 	 'Context: ' + (ctx.params.contactName || ''));
-// }
-
 function notFound() {
   console.log('Page not found. Please re-type your URL. KTHANKS.');
 }
